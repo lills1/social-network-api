99d91e60956e2dc5b2e6202e7f9b83d03549f738
lills1 <lilly.stephenson4@gmail.com>
2022-01-08 11:45:37 +1100

    added the routes for reaction and friends

--- Main/routes/api/thoughtRoutes.js
+++ Main/routes/api/thoughtRoutes.js
@@ -4,7 +4,9 @@ const {
   getSingleThought,
   createThought,
   deleteThought,
-  updateThought
+  updateThought,
+  createReaction,
+  deleteReaction
 } = require('../../controllers/thoughtController.js');
 
 // /api/courses
@@ -12,5 +14,14 @@ router.route('/').get(getThoughts).post(createThought);
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
 // /api/courses/:courseId
 
+// http://localhost:3001/api/thoughts/:id/reactions
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction)
+
+// http://localhost:3001/api/thoughts/:id/reactions/:reactionId
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
 
 module.exports = router;
--- Main/routes/api/userRoutes.js
+++ Main/routes/api/userRoutes.js
@@ -4,16 +4,22 @@ const {
   getSingleUser,
   createUser,
   deleteUser,
-  updateUser
+  updateUser,
+  addFriend,
+  deleteFriend
 
 } = require('../../controllers/userController');
 
-// /api/students
+// /api/user
 router.route('/').get(getUser).post(createUser);
 
-// /api/students/:studentId
+// /api/students/:userId
 router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
-
+// http://localhost:3001/api/users/:id/friends/:friendId
+router
+  .route('/:id/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
 
 module.exports = router;
